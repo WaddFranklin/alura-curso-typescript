68d41e65e82117a4d412b58917bbd93c5399bee9
WaddFranklin <waddinsohn@gmail.com>
2022-09-17 11:15:05 -0300

    Alterando a estrutura do projeto typescript

--- app/dist/js/models/negociacao.js
+++ app/dist/js/models/negociacao.js
@@ -4,18 +4,18 @@ export class Negociacao {
         this.quantidade = quantidade;
         this.valor = valor;
     }
+    get volume() {
+        return this.quantidade * this.valor;
+    }
     get data() {
         const data = new Date(this._data.getTime());
         return data;
     }
-    get volume() {
-        return this.quantidade * this.valor;
-    }
     static criaDe(dataString, quantidadeString, valorString) {
         const exp = /-/g;
-        const data = new Date(dataString.replace(exp, ','));
+        const date = new Date(dataString.replace(exp, ','));
         const quantidade = parseInt(quantidadeString);
-        const valor = parseInt(valorString);
-        return new Negociacao(data, quantidade, valor);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
     }
 }
--- app/src/models/negociacoes.ts
+++ app/src/models/negociacoes.ts
@@ -1,9 +1,9 @@
-import { Negociacao } from "./negociacao.js";
+import { Negociacao } from './negociacao.js';
 
 export class Negociacoes {
     private negociacoes: Negociacao[] = [];
 
-    public adiciona(negociacao: Negociacao): void {
+    public adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
@@ -11,5 +11,3 @@ export class Negociacoes {
         return this.negociacoes;
     }
 }
-
-const negociacoes = new Negociacoes();
